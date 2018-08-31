090d786765f4012ff97e7e2498fafb20caeaab0d
DoHoa <lengocthe54@gmail.com>
2018-08-31 21:04:16 +0700

    LNT commit

--- app/src/main/java/com/example/dohoa/xmlparser/activity/MainActivity.java
+++ app/src/main/java/com/example/dohoa/xmlparser/activity/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.dohoa.xmlparser.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.dohoa.xmlparser.R;
+import com.example.dohoa.xmlparser.adapter.NewsAdapter;
 import com.example.dohoa.xmlparser.model.News;
 import com.example.dohoa.xmlparser.model.XMLAsync;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements XMLAsync.XMLParserCallBack {
+
+    private RecyclerView rvListNews;
+    private ArrayList<News> listNews = new ArrayList<>();
+    private NewsAdapter newsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +26,22 @@ public class MainActivity extends AppCompatActivity implements XMLAsync.XMLParse
         String link = "https://www.24h.com.vn/upload/rss/bongda.rsshttps://www.24h.com.vn/upload/rss/bongda.rss";
         XMLAsync async = new XMLAsync(this);
         async.execute(link);
+
+        initViews();
+    }
+
+    private void initViews() {
+        rvListNews = findViewById(R.id.rv_list_news);
+        rvListNews.setLayoutManager(new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.VERTICAL, false));
+        rvListNews.setHasFixedSize(true);
+
+        newsAdapter = new NewsAdapter(this, listNews);
+        rvListNews.setAdapter(newsAdapter);
     }
 
     @Override
-    public void onParseResult(ArrayList<News> listNews) {
-
+    public void onParseResult(ArrayList<News> newListNews) {
+        listNews.addAll(newListNews);
     }
 }
--- app/src/main/java/com/example/dohoa/xmlparser/adapter/NewsAdapter.java
+++ app/src/main/java/com/example/dohoa/xmlparser/adapter/NewsAdapter.java
@@ -1,26 +1,43 @@
 package com.example.dohoa.xmlparser.adapter;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.example.dohoa.xmlparser.R;
 import com.example.dohoa.xmlparser.databinding.ItemNewsBinding;
+import com.example.dohoa.xmlparser.model.News;
+
+import java.util.ArrayList;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private ArrayList<News> listNews;
+    private LayoutInflater inflater;
+
+    public NewsAdapter(Context context, ArrayList<News> listNews) {
+        this.listNews = listNews;
+        inflater = LayoutInflater.from(context);
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemNewsBinding binding = DataBindingUtil.inflate(inflater, R.layout.item_news, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.bindData(listNews.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listNews.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -29,6 +46,12 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         public ViewHolder(ItemNewsBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+        }
+
+        void bindData(News news) {
+            binding.tvTitle.setText(news.getTitle());
+            binding.tvDesc.setText(news.getDescription());
+            binding.tvPubDate.setText(news.getPubDate());
         }
     }
 }
